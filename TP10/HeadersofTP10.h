e43c37327a4333bd76d28977125ed182adc1753c
Ebatemarco <72512561+Ebatemarco@users.noreply.github.com>
2020-10-22 20:07:29 -0300

    Add files via upload

---
+++ TP10/HeadersofTP10.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   HeadersofTP10.h
+ * Author: ebatemarco
+ *
+ * Created on 22 de octubre de 2020, 19:34
+ */
+
+#ifndef HEADERSOFTP10_H
+#define HEADERSOFTP10_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HEADERSOFTP10_H */
+
